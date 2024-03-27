a3cd5766d83ecde6339b6f8f988d5c214a717362
Alioune <aliounealdembaseck@gmail.com>
2024-03-27 15:52:01 +0000

    creation des modules et du dossier shared

---
+++ src/app/gestionnaire/gestionnaire.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GestionnaireRoutingModule } from './gestionnaire-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GestionnaireRoutingModule
+  ]
+})
+export class GestionnaireModule { }
---
+++ src/app/proprietaire/proprietaire.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProprietaireRoutingModule } from './proprietaire-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProprietaireRoutingModule
+  ]
+})
+export class ProprietaireModule { }
