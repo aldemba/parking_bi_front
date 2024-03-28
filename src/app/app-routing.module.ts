c4284151e64a19de9c43b9c3de450cded3df0fcf
Alioune <aliounealdembaseck@gmail.com>
2024-03-28 13:31:56 +0000

    creation de routes dans app et de quelques components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'securiry', loadChildren: ()=> import('./securite/securite.module').then(m=>m.SecuriteModule)},
+  {path:'client', loadChildren: ()=> import('./client/client.module').then(m=>m.ClientModule)},
+  {path:'superadmin', loadChildren: ()=> import('./gestionnaire/gestionnaire.module').then(m=>m.GestionnaireModule)},
+  {path:'admin', loadChildren: ()=> import('./proprietaire/proprietaire.module').then(m=>m.ProprietaireModule)},
+  {path:'**',component:NotFoundComponent}
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/securite/securite.module.ts
+++ src/app/securite/securite.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SecuriteRoutingModule } from './securite-routing.module';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ConnexionComponent,
+    InscriptionComponent
+  ],
   imports: [
     CommonModule,
     SecuriteRoutingModule
