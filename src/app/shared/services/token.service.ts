c64615f1bbad2c294d1ea64d1f394ac3043e18ec
Alioune <aliounealdembaseck@gmail.com>
2024-04-02 23:19:25 +0000

    toujours dans la logique de login

--- src/app/shared/services/token.service.ts
+++ src/app/shared/services/token.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  constructor() { }
+
+  constructor(private router:Router) { }
+
+    
+  saveIdInStorage(id: number){  localStorage.setItem("id",id.toString());    }
+
+  getIdFromStorage(){  return localStorage.getItem("id");    }
+
+  saveTokenInStorage(token: string){  localStorage.setItem("token", token);    }
+
+  getTokenFromStorage(): string | null{  return localStorage.getItem("token") }
+
+
+  isLogged():boolean{
+
+    const token=localStorage.getItem('token')
+
+    return !!token
+  }
+
+
+  clearTokenAndId()
+  {
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    // location.reload();
+     this.router.navigate(["security/login"])
+  }
+
+
+
+
+
 }
