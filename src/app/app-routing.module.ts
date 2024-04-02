d10f57a5cd4b5db0d1a5aeaf0f2a4f632d43e5d7
Alioune <aliounealdembaseck@gmail.com>
2024-04-02 22:00:26 +0000

    creation services et models pour la connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  {path:'securiry', loadChildren: ()=> import('./securite/securite.module').then(m=>m.SecuriteModule)},
+  {path:'security', loadChildren: ()=> import('./securite/securite.module').then(m=>m.SecuriteModule)},
   {path:'client', loadChildren: ()=> import('./client/client.module').then(m=>m.ClientModule)},
-  {path:'superadmin', loadChildren: ()=> import('./gestionnaire/gestionnaire.module').then(m=>m.GestionnaireModule)},
-  {path:'admin', loadChildren: ()=> import('./proprietaire/proprietaire.module').then(m=>m.ProprietaireModule)},
-  {path:'', redirectTo:'client',pathMatch:'full'},
+  // {path:'superadmin', loadChildren: ()=> import('./gestionnaire/gestionnaire.module').then(m=>m.GestionnaireModule)},
+  // {path:'admin', loadChildren: ()=> import('./proprietaire/proprietaire.module').then(m=>m.ProprietaireModule)},
+  {path:'', redirectTo:'security',pathMatch:'full'},
   {path:'**',component:NotFoundComponent}
   
 
