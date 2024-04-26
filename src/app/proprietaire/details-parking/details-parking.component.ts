87a7f2c75c46d1f71b9aa7c02310e977a3ac14c9
Alioune <aliounealdembaseck@gmail.com>
2024-04-26 13:35:53 +0000

    icone danger assurance et visite technique

--- src/app/proprietaire/details-parking/details-parking.component.ts
+++ src/app/proprietaire/details-parking/details-parking.component.ts
@@ -6,6 +6,7 @@ import { VoituresService } from 'src/app/shared/services/voitures.service';
 import { ToastrService } from 'ngx-toastr';
 
 import Swal from 'sweetalert2';
+import { Voiture } from 'src/app/shared/models/voiture';
 
 @Component({
   selector: 'app-details-parking',
@@ -25,6 +26,10 @@ export class DetailsParkingComponent {
   searchTerm: any;
   idbis:number=0;
   etatSwitch: boolean = false;
+  test:boolean=true
+  
+
+  // objets:Voiture[] = [];
 
   constructor(private activatedroute:ActivatedRoute, private detailserv:DetailsService, private voitureserv: VoituresService, private router:Router, private toastr: ToastrService) { }
 
@@ -40,12 +45,13 @@ export class DetailsParkingComponent {
           this.voitures = this.voitures.filter((voiture: any) => voiture.isVisible ==1);
           this.filtres = this.voitures.filter((voiture: any) => voiture.isVisible ==1);
           this.totalLength=this.voitures.length;
-          // console.log("test",this.voitures);
+          // this.objets.push(data.voitures)
+           console.log("test",this.voitures);
           // console.log("id",this.idbis);
           
-          this.voitures.forEach((voiture: any) => {
-            voiture.etatSwitch = voiture.etat === 'DISPONIBLE';
-          });
+          // this.voitures.forEach((voiture: any) => {
+          //   voiture.etatSwitch = voiture.etat === 'DISPONIBLE';
+          // });
         })
       }
     )
@@ -55,6 +61,9 @@ export class DetailsParkingComponent {
     
       this.all=data.all;
     })
+
+    // this.objets = this.verifierDifference(this.objets);
+    // console.log("a", this.objets);
 
   }
   
@@ -69,6 +78,58 @@ afficherDetailsVoiture(voiture: any) {
 showSuccess() {
   this.toastr.success('La voiture a été supprimé avec succès!', 'Suppression!');
 }
+
+
+getDifferenceInDays(dateString: string): boolean {
+  const today = new Date();
+  const expirationDate = new Date(dateString);
+  const differenceInMs = expirationDate.getTime() - today.getTime();
+  const differenceInDays = Math.ceil(differenceInMs / (1000 * 60 * 60 * 24));
+  return differenceInDays <= 7;
+}
+
+
+// // Fonction pour calculer la différence entre deux dates en jours
+//  differenceEnJours(date1:any, date2:any) {
+//   const unJour = 24 * 60 * 60 * 1000; // Nombre de millisecondes dans un jour
+//   const diffTemps = Math.abs(date1.getTime() - date2.getTime());;
+//   return Math.round(diffTemps / unJour);
+// }
+
+
+// Fonction principale pour vérifier la différence et ajouter ou supprimer un attribut
+// verifierDifference(objets: any) {
+//   let dateDuJour = new Date(); // Date actuelle
+
+//   console.log("table",objets);
+  
+
+//    objets[0].forEach((objet: any) => {
+//     let dateFinVisite = objet.visite.date_fin_visite;
+//     let dateFinAssurance = objet.assurance.date_fin;
+
+//     console.log(dateFinVisite);
+//     console.log(dateFinAssurance);
+    
+    
+
+//   //   // Calcul de la différence en jours
+//   //   // const differenceVisite: number = Math.ceil((dateFinVisite.getTime() - dateDuJour.getTime()) / (1000 * 3600 * 24));
+//   //   // const differenceAssurance: number = Math.ceil((dateFinAssurance.getTime() - dateDuJour.getTime()) / (1000 * 3600 * 24));
+
+//   //   // // Vérification de la différence et ajout ou suppression de l'attribut
+//   //   // if (differenceVisite < 7 || differenceAssurance < 7) {
+//   //   //   objet = Object.assign({}, objet, {
+//   //   //     "differenceInf7Jours": true
+//   //   //   });
+//   //   // } else {
+//   //   //   delete objet.differenceInf7Jours;
+//   //   // }
+//    });
+
+//   return objets; // Retourne les objets modifiés
+// }
+
 
 
   // updateEtat(voiture: any) {
@@ -94,30 +155,30 @@ showSuccess() {
   //   });
   // }
 
-  updateEtat(voiture: any) {
-    // Afficher la boîte modale SweetAlert pour demander la confirmation
-    Swal.fire({
-      title: 'Êtes-vous sûr?',
-      text: "Voulez-vous vraiment changer l'état de cette voiture?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Oui, changer l\'état!',
-      cancelButtonText: 'Annuler'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Inverser l'état de la voiture
-        const nouvelEtat = voiture.etat === 'DISPONIBLE' ? 'INDISPONIBLE' : 'DISPONIBLE';
-        voiture.etat = nouvelEtat; // Met à jour l'état
-        this.voitureserv.changeState(voiture).subscribe({
-          // Vous pouvez gérer les réponses de votre requête ici
-          // next: (data:any) => { alert(data)},
-          // error: (err:any) => { alert(err)}
-        });
-      }
-    });
-  }
+  // updateEtat(voiture: any) {
+  //   // Afficher la boîte modale SweetAlert pour demander la confirmation
+  //   Swal.fire({
+  //     title: 'Êtes-vous sûr?',
+  //     text: "Voulez-vous vraiment changer l'état de cette voiture?",
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonColor: '#3085d6',
+  //     cancelButtonColor: '#d33',
+  //     confirmButtonText: 'Oui, changer l\'état!',
+  //     cancelButtonText: 'Annuler'
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+  //       // Inverser l'état de la voiture
+  //       const nouvelEtat = voiture.etat === 'DISPONIBLE' ? 'INDISPONIBLE' : 'DISPONIBLE';
+  //       voiture.etat = nouvelEtat; // Met à jour l'état
+  //       this.voitureserv.changeState(voiture).subscribe({
+  //         // Vous pouvez gérer les réponses de votre requête ici
+  //         // next: (data:any) => { alert(data)},
+  //         // error: (err:any) => { alert(err)}
+  //       });
+  //     }
+  //   });
+  // }
   
   
   
