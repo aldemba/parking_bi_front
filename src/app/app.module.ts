e9926e7f348c2cfb53185a60ae1ce2cd07e91931
Alioune <aliounealdembaseck@gmail.com>
2024-06-10 11:25:35 +0000

    hash router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
 import { TokenInterceptorProvider } from './shared/services/token-interceptor.service';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,10 @@ import { TokenInterceptorProvider } from './shared/services/token-interceptor.se
     ToastrModule.forRoot(),
     BrowserAnimationsModule, // required animations module
   ],
-  providers: [TokenInterceptorProvider],
+  providers: [
+    TokenInterceptorProvider,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
